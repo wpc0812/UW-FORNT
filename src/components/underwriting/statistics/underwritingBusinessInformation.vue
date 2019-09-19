6ab0ff640987c2f3bff94f2ec7f2409a19c1aaeb
Wpc <741748834@qq.com>
2019-09-19 18:25:10 +0800

    开发

--- src/components/underwriting/statistics/underwritingBusinessInformation.vue
+++ src/components/underwriting/statistics/underwritingBusinessInformation.vue
@@ -2,92 +2,89 @@
   <div>
     <!-- 信息展示 -->
     <el-card class="circular mt4 shadow">
-      <el-collapse v-model="activeNames">
-        <el-collapse-item name="1">
+      
           <el-table
             stripe
-            :data="InsuranceApplication"
+            :data="tableList.costTable"
             tooltip-effect="dark"
             :cell-style="{'text-align': 'center'}"
             :header-cell-style="{'text-align': 'center'}"
             :header-cell-class-name="'table-header-bg'"
           >
-            <el-table-column prop="branchOffice" label></el-table-column>
-            <el-table-column prop="insurancePeriod" label="保险期限"></el-table-column>
-            <el-table-column prop="insuredName" label="被保险人名称"></el-table-column>
-            <el-table-column prop="initialDate" label="初登日期"></el-table-column>
-            <el-table-column prop="underwriteUser" width="115" label="核保人员"></el-table-column>
-            <el-table-column prop="correctionType" label="批改类型"></el-table-column>
-            <el-table-column prop="underwriteState" label="核保状态"></el-table-column>
+            <el-table-column prop="name" ></el-table-column>
+            <el-table-column prop="price" label="交强险比率%"></el-table-column>
+            <el-table-column prop="price" label="金额"></el-table-column>
+            <el-table-column prop="price" label="商业险比率%"></el-table-column>
+            <el-table-column prop="price" label="金额"></el-table-column>
           </el-table>
-        </el-collapse-item>
-      </el-collapse>
-    </el-card>
-    <el-card class="circular mt4 shadow">
-      <el-collapse v-model="activeNames">
-        <el-collapse-item name="1">
+
           <el-table
             stripe
-            :data="InsurancePolicy"
+            :data="tableList.businissType"
             tooltip-effect="dark"
             :cell-style="{'text-align': 'center'}"
             :header-cell-style="{'text-align': 'center'}"
             :header-cell-class-name="'table-header-bg'"
           >
-            <el-table-column prop="branchOffice" label="分公司"></el-table-column>
-            <el-table-column prop="insurancePeriod" label="保险期限"></el-table-column>
-            <el-table-column prop="insuredName" label="被保险人名称"></el-table-column>
-            <el-table-column prop="initialDate" label="初登日期"></el-table-column>
-            <el-table-column prop="underwriteUser" width="115" label="核保人员"></el-table-column>
-            <el-table-column prop="correctionType" label="批改类型"></el-table-column>
-            <el-table-column prop="underwriteState" label="核保状态"></el-table-column>
+            <el-table-column prop="name" label="业务类别:"></el-table-column>
+            <el-table-column prop="price" label="A"></el-table-column>
+            <el-table-column prop="price" label="B"></el-table-column>
+            <el-table-column prop="price" label="C"></el-table-column>
+            <el-table-column prop="price" label="D"></el-table-column>
+            <el-table-column prop="price" label="E"></el-table-column>
           </el-table>
-        </el-collapse-item>
-      </el-collapse>
-    </el-card>
-    <el-card class="circular mt4 shadow">
-      <el-collapse v-model="activeNames">
-        <el-collapse-item name="1">
+
+          <el-form label-width="160px">
+            <el-row class="mt4">
+              <el-col :span="8">
+                <el-form-item label="折扣和后续费实际比率%">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+               <el-col :span="8">
+                <el-form-item label="最低净保费:">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+               <el-col :span="8">
+                <el-form-item label="原业务类别:">
+                  <el-input></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
           <el-table
             stripe
-            :data="singleNum"
+            :data="tableList.discount"
             tooltip-effect="dark"
             :cell-style="{'text-align': 'center'}"
             :header-cell-style="{'text-align': 'center'}"
             :header-cell-class-name="'table-header-bg'"
           >
-            <el-table-column prop="branchOffice" label="分公司"></el-table-column>
-            <el-table-column prop="insurancePeriod" label="保险期限"></el-table-column>
-            <el-table-column prop="insuredName" label="被保险人名称"></el-table-column>
-            <el-table-column prop="initialDate" label="初登日期"></el-table-column>
-            <el-table-column prop="underwriteUser" width="115" label="核保人员"></el-table-column>
-            <el-table-column prop="correctionType" label="批改类型"></el-table-column>
-            <el-table-column prop="underwriteState" label="核保状态"></el-table-column>
+            <el-table-column prop="name" label='折扣和手续费率:'></el-table-column>
+            <el-table-column prop="insurancePeriod" label="上限%"></el-table-column>
+            <el-table-column prop="insuredName" label="差值%"></el-table-column>
+            <el-table-column prop="initialDate" label="业务类型"></el-table-column>
           </el-table>
-        </el-collapse-item>
-      </el-collapse>
-    </el-card>
-    <el-card class="circular mt4 shadow">
-      <el-collapse v-model="activeNames">
-        <el-collapse-item name="1">
           <el-table
             stripe
-            :data="statisticalResult"
+            :data="tableList.ProfitLossTips"
             tooltip-effect="dark"
             :cell-style="{'text-align': 'center'}"
             :header-cell-style="{'text-align': 'center'}"
             :header-cell-class-name="'table-header-bg'"
           >
-            <el-table-column prop="submissionNumber" label="核保提交数量"></el-table-column>
-            <el-table-column prop="artificialUnderwritingNumber" label="人工核保通过数量"></el-table-column>
-            <el-table-column prop="autoUnderwritingNumber" label="自动核保通过数量"></el-table-column>
-            <el-table-column prop="artificialUnderwritingPassingRate" label="人工核保通过率%"></el-table-column>
-            <el-table-column prop="autoUnderwritingPassingRate" label="自动核保通过率%"></el-table-column>
-            <el-table-column prop="aumUnderwritingPassingRate" label="总核保通过率%"></el-table-column>
+            <el-table-column prop="name" label='精算盈亏提示:'></el-table-column>
+            <el-table-column prop="type" label="业务类型"></el-table-column>
+            <el-table-column prop="insuredName" label="险别"></el-table-column>
           </el-table>
-        </el-collapse-item>
-      </el-collapse>
+          <el-row>
+            <el-col :span="24" class="text-center mt10">
+              <el-button  size="mini" @click="colseWidow">关闭</el-button>
+            </el-col>
+          </el-row>
     </el-card>
+   
   </div>
 </template>
 <script>
@@ -99,12 +96,13 @@ export default {
   components: {},
   data() {
     return {
-      activeNames: ["1"],
-      InsuranceApplication: [],
-      InsurancePolicy: [],
-      singleNum: [],
-      statisticalResult: [],
-      arraystate:[]
+      tableList:{
+        costTable:[],
+        businissType:[],
+        discount:[],
+        ProfitLossTips:[]
+
+      }
     };
   },
 
@@ -113,83 +111,55 @@ export default {
   },
 
   methods: {
-    //多选框
-    changecheckbox() {
-    },
-    //导出
-    rtReportedchu() {
-      let uwMotorcadeMainVO = this.UwMotorcadeMainVO;
-      uwMotorcadeMainVO.state = this.arraystate.join();
-      let _url = this.HOST + this.$url.underwritingStatisticsExport;
-      utils.axiosDown(_url, uwMotorcadeMainVO);
-    },
-    // 查询
-    query() {
-      let uwMotorcadeMainVO = this.UwMotorcadeMainVO;
-      uwMotorcadeMainVO.state = this.arraystate.join();
-      this.$fetch
-        .post(this.HOST + this.$url.underwritingStatistics, uwMotorcadeMainVO)
-        .then(res => {
-          if (res) {
-            this.flag = true;
-            this.flag1 = false;
-            this.InsuranceApplication = res.proposalList;
-            this.InsurancePolicy = res.policyList;
-            this.singleNum = res.endorsementList;
-            this.totalnum = res.proposalTotal;
-            this.totalnum1 = res.policyTotal;
-            this.totalnum2 = res.endorsementTotal;
-          }
-        });
-    },
-    //统计结果
-    statisticalResults() {
-      let uwMotorcadeMainVO = this.UwMotorcadeMainVO;
-      uwMotorcadeMainVO.state = this.arraystate.join();
-      this.$fetch
-        .post(
-          this.HOST + this.$url.underwritingStatisticsResult,
-          uwMotorcadeMainVO
-        )
-        .then(res => {
-          if (res) {
-            this.flag = false;
-            this.flag1 = true;
-            this.statisticalResult.push(res);
-          }
-        });
-    },
-    //业务号
-    BusinessNum(row) {}
+    // 关闭弹框
+    colseWidow(){
+      window.close()
+    }
+    
+    
   },
   created() {
-    this.flag = false;
-    this.flag1 = false;
+    this.tableList={
+        costTable:[
+          {name:'缺省费用',price: '0.00000'},
+          {name:'实际费用',price: '0.00000'},
+          {name:'交强业务类型费用上限%',price: ''},
+          {name:'商业业务类型费用上限%',price: ''},
+
+
+        ],
+        businissType:[
+          {name:'目标控制比例%',price: '0.00000'},
+          {name:'精算口径比例%',price: '0.00000'},
+          {name:'经验口径比例%',price: '0.00000'},
+          {name:'精算口径差距%',price: '0.00000'},
+          {name:'经验口径差距%',price: '0.00000'},
+
+
+        ],
+        discount:[
+          {name:'经验口径'},
+          {name:'精算口径'},
+          {name:'经验盈亏险别提示'},
+
+        ],
+        ProfitLossTips:[
+          { type:'A高盈利'},
+          { type:'B微 利'},
+          { type:'C保 本'},
+          { type:'D微 亏'},
+          { type:'E高 亏'},
+
+        ]
+
+      }
   }
 };
 </script>
 <style scoped>
-.el-collapse {
-  border: 0;
-}
-.el-collapse >>> .el-collapse-item__wrap,
-.el-collapse >>> .el-collapse-item__header {
-  border: 0;
-}
-.card-title {
-  color: #717385;
-  font-size: 15px;
-  font-weight: 700;
-}
-.el-card >>> .el-card__body {
+
+ .el-card__body {
   padding: 10px 20px;
-}
-.title-blue-bar {
-  width: 10px;
-  height: 20px;
-  background-color: #409eff;
-  border-radius: 8px;
-  margin-right: 10px;
 }
 .circular {
   border-radius: 8px;
